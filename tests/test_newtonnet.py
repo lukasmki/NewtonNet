13af94083648149a441e91742a15e1adc33e5536
Lukas <lukasarus@gmail.com>
2023-01-13 11:39:34 -0800

    Added PairProperty forward test

--- tests/test_newtonnet.py
+++ tests/test_newtonnet.py
@@ -5,9 +5,17 @@ from newtonnet.models.newtonnet import AtomicProperty, PairProperty
 
 class TestAtomicProperty(unittest.TestCase):
     def testForward(self):
-        x = torch.ones((10, 6, 128))
+        a = torch.ones((10, 6, 128))
         m = AtomicProperty(128, torch.sigmoid, 0.0)
-        pred = m(x)
+        pred = m.forward(a)
+        assert pred.shape == torch.Size([10, 6, 1])
+
+class TestPairProperty(unittest.TestCase):
+    def testForward(self):
+        mij = torch.ones((10, 6, 6, 128))
+        m = PairProperty(128, torch.sigmoid)
+        pred = m.forward(mij)
+        assert pred.shape == torch.Size([10, 6, 6, 1])
 
 if (__name__=="__main__"):
     unittest.main()
