9d6fd4a7dd4a6ca168d1ed14b0e67e906ec2a720
lukasmki <lukasarus@gmail.com>
2023-02-28 18:20:32 -0800

    formatting

--- tests/test_newtonnet.py
+++ tests/test_newtonnet.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 from newtonnet.models.newtonnet import AtomicProperty, PairProperty, NewtonNet
 
+
 class TestAtomicProperty(unittest.TestCase):
     def test_forward(self):
         a = torch.ones((10, 6, 128))
@@ -10,12 +11,14 @@ class TestAtomicProperty(unittest.TestCase):
         pred = m.forward(a)
         assert pred.shape == torch.Size([10, 6, 1])
 
+
 class TestPairProperty(unittest.TestCase):
     def test_forward(self):
-        mij = torch.ones((10, 6, 6, 128))
+        mij = torch.ones((10, 6, 5, 128))
         m = PairProperty(128, torch.sigmoid)
         pred = m.forward(mij)
-        assert pred.shape == torch.Size([10, 6, 6, 1])
+        assert pred.shape == torch.Size([10, 6, 5, 1])
 
-if (__name__=="__main__"):
+
+if __name__ == "__main__":
     unittest.main()
